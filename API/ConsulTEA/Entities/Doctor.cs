84a70c0929686b4e5c342edba74fcc9e9c5994ec
ANDREYPAD3\andre <andreyreboredo@usp.br>
2025-10-14 15:54:53 -0300

    add doctor login endpoint

---
+++ API/ConsulTEA/Entities/Doctor.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsulTEA.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Crm { get; set; }
+        public string Cpf { get; set; }
+        public string Specialty { get; set; }
+    }
+}
