504bc9ea089ac5d3a0c4f96951cbf2067084ccaa
Heymity <gabriel.prscavone@gmail.com>
2025-10-14 17:05:26 -0300

    Working Front-API login connection

---
+++ API/ConsulTEA/Authentication/TokenProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ConsulTEA.Authentication;
+
+public class TokenProvider
+{
+    public string GenerateToken(string Cpf)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = "veryveryverySecretkeyThatNoOneKnowsAboutShhhhhhDontShareTHis"u8.ToArray();
+
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Jti, Cpf)
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddHours(24),
+            Issuer = "https://localhost:5001",
+            Audience = "https://localhost:5001",
+            SigningCredentials =
+                new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
--- API/ConsulTEA/Program.cs
+++ API/ConsulTEA/Program.cs
@@ -1,3 +1,7 @@
+using ConsulTEA.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<TokenProvider>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        b =>
+        {
+            b.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = "https://localhost:5001",
+        ValidAudience = "https://localhost:5001",
+        IssuerSigningKey =
+            new SymmetricSecurityKey("veryveryverySecretkeyThatNoOneKnowsAboutShhhhhhDontShareTHis"u8.ToArray()),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+    };
+});
 
 var app = builder.Build();
 
@@ -16,6 +49,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//Cors allow any
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
