feb04a268e79ca7b9cda685744407a3809b651fd
K4iwer <lucassposocavalari@usp.br>
2025-10-21 16:33:34 -0300

    working on the patient endpoints

---
+++ API/ConsulTEA/Entities/Patient.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsulTEA.Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+    }
+}
