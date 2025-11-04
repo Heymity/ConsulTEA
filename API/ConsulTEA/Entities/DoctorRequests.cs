630d95437c2cf485f36479251e5877bb48c27179
ANDREYPAD3\andre <andreyreboredo@usp.br>
2025-11-04 17:29:10 -0300

    doctor register request

--- API/ConsulTEA/Entities/DoctorRequests.cs
+++ API/ConsulTEA/Entities/DoctorRequests.cs
@@ -1,6 +1,6 @@
 ﻿namespace ConsulTEA.Entities
 {
     public record DoctorLogInRequest (string Cpf, string Password);
-    public record DoctorRegisterRequest(string Name, string Cpf, string Crm, string Specialty);
+    public record DoctorRegisterRequest(string Name, string Cpf, string Crm, string Specialty, string Email, string Password);
     
 }
