745df89f531742ff2be67bb17cc9d3eb9fed59f6
Heymity <gabriel.prscavone@gmail.com>
2025-11-11 16:20:45 -0300

    Appointment API with correct authentication access

--- API/ConsulTEA/Controllers/AppointmentController.cs
+++ API/ConsulTEA/Controllers/AppointmentController.cs
@@ -1,7 +1,9 @@
-﻿using ConsulTEA.Entities;
+﻿using System.Security.Claims;
+using ConsulTEA.Entities;
 using ConsulTEA.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 using Npgsql;
 
 namespace ConsulTEA.Controllers
@@ -36,7 +38,8 @@ namespace ConsulTEA.Controllers
                 MedicationInUse = reader.GetString(reader.GetOrdinal("medication_in_use")),
                 EvolutionSummary = reader.GetString(reader.GetOrdinal("evolution_summary")),
                 NextSteps = reader.GetString(reader.GetOrdinal("next_steps")),
-                CreatedAt = reader.GetDateTime(reader.GetOrdinal("created_at"))
+                CreatedAt = reader.GetDateTime(reader.GetOrdinal("created_at")),
+                UpdatedAt = reader.GetDateTime(reader.GetOrdinal("updated_at")),
             };
         }
 
@@ -48,12 +51,18 @@ namespace ConsulTEA.Controllers
 
             try
             {
+                var cpfClaim = User.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Jti);
+                if (cpfClaim is null) return Unauthorized("Não está logado como médico");
+
+                var doctorCpf = cpfClaim.Value;
+                
                 // Creates/checks db conection
                 await using var conn = await _dbService.GetConnection();
 
-                const string query = "SELECT * FROM bd_dados_exame WHERE id_appointment = @id";
+                const string query = "SELECT * FROM bd_dados_exame WHERE id_appointment = @id AND id_doctor = (SELECT bd_doctor_identification.id_doctor FROM bd_doctor_identification WHERE cpf = @doctor_cpf)";
                 await using var cmd = new NpgsqlCommand(query, conn);
                 cmd.Parameters.AddWithValue("id", id);
+                cmd.Parameters.AddWithValue("doctor_cpf", doctorCpf);
 
                 await using var reader = await cmd.ExecuteReaderAsync();
 
@@ -79,22 +88,27 @@ namespace ConsulTEA.Controllers
 
             try
             {
+                var cpfClaim = User.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Jti);
+                if (cpfClaim is null) return Unauthorized("Não está logado como médico");
+
+                var doctorCpf = cpfClaim.Value;
+                
                 // Creates/checks db conection
                 await using var conn = await _dbService.GetConnection();
 
-                var query = "SELECT * FROM bd_dados_exame WHERE id_patient = @id";
+                const string query = "SELECT * FROM bd_dados_exame WHERE id_patient = @id AND id_doctor = (SELECT bd_doctor_identification.id_doctor FROM bd_doctor_identification WHERE cpf = @doctor_cpf)";
                 await using var cmd = new NpgsqlCommand(query, conn);
                 cmd.Parameters.AddWithValue("id", id);
+                cmd.Parameters.AddWithValue("doctor_cpf", doctorCpf);
 
                 await using var reader = await cmd.ExecuteReaderAsync();
 
                 var appointments = new List<Appointment>();
-
-                if (await reader.ReadAsync())
+                
+                while (await reader.ReadAsync())
                 {
                     appointments.Add(ReadAppointment(reader));
                 }
-
                 return Ok(appointments);
             }
             catch (Exception ex)
@@ -105,28 +119,35 @@ namespace ConsulTEA.Controllers
         }
 
         // Returns the Data of an Appointment by the Doctor Id
-        [HttpGet("get/Doctor/{id}")]
-        public async Task<IActionResult> GetAppointmentByDoctorId(int id)
+        [HttpGet("get/Doctor")]
+        public async Task<IActionResult> GetAppointmentByDoctorId()
         {
             _logger.LogInformation("Get Appointment Request by Doctor Id");
 
             try
             {
+                var cpfClaim = User.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Jti);
+                if (cpfClaim is null) return Unauthorized("Não está logado como médico");
+
+                var doctorCpf = cpfClaim.Value;
+                
                 // Creates/checks db conection
                 await using var conn = await _dbService.GetConnection();
 
-                var query = "SELECT * FROM bd_dados_exame WHERE id_doctor = @id";
+                const string query = "SELECT * FROM bd_dados_exame WHERE id_doctor = (SELECT bd_doctor_identification.id_doctor FROM bd_doctor_identification WHERE cpf = @doctor_cpf)";
                 await using var cmd = new NpgsqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("id", id);
+                cmd.Parameters.AddWithValue("doctor_cpf", doctorCpf);
 
                 await using var reader = await cmd.ExecuteReaderAsync();
 
                 var appointments = new List<Appointment>();
 
-                if (await reader.ReadAsync())
+                while (await reader.ReadAsync())
                 {
                     appointments.Add(ReadAppointment(reader));
                 }
+
+                
 
                 return Ok(appointments);
             }
@@ -139,7 +160,7 @@ namespace ConsulTEA.Controllers
 
         // Inserts new appointment
         [HttpPost("new")]
-        public async Task<IActionResult> InsertNewAppointment(AppointmentInsertRequest request)
+        public async Task<IActionResult> InsertNewAppointment([FromBody] AppointmentInsertRequest request)
         {
             _logger.LogInformation("Insert Appointment Request");
 
@@ -148,44 +169,47 @@ namespace ConsulTEA.Controllers
                 // Creates/checks db conection
                 await using var conn = await _dbService.GetConnection();
 
-                if (request.IdDoctor == null || request.IdPatient == null)
+                if (request.IdPatient == null)
                     return NotFound("É necessário passar um médico e um paciente");
 
-                var query = """
-                    INSERT INTO bd_dados_exame (
-                        id_doctor, 
-                        id_patient, 
-                        date, 
-                        main_complaint, 
-                        behavior_observation, 
-                        communication_notes, 
-                        sensory_notes, 
-                        social_interaction, 
-                        medication_in_use, 
-                        evolution_summary, 
-                        next_steps, 
-                        created_at
-                    )
-                    VALUES (
-                        @idDoctor, 
-                        @idPatient, 
-                        @date, 
-                        @mainComplaint, 
-                        @behaviorObservation, 
-                        @communicationNotes, 
-                        @sensoryNotes, 
-                        @socialInteraction, 
-                        @medicationInUse, 
-                        @evolutionSummary, 
-                        @nextSteps, 
-                        @createdAt
-                    )
-                """;
+                var cpfClaim = User.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Jti);
+                if (cpfClaim is null) return Unauthorized("Não está logado como médico");
+
+                var doctorCpf = cpfClaim.Value;
+                
+                const string query = """
+                                         INSERT INTO bd_dados_exame (
+                                             id_doctor, 
+                                             id_patient, 
+                                             date, 
+                                             main_complaint, 
+                                             behavior_observation, 
+                                             communication_notes, 
+                                             sensory_notes, 
+                                             social_interaction, 
+                                             medication_in_use, 
+                                             evolution_summary, 
+                                             next_steps
+                                         )
+                                         VALUES (
+                                             (SELECT bd_doctor_identification.id_doctor FROM bd_doctor_identification WHERE cpf = @doctor_cpf), 
+                                             @idPatient, 
+                                             @date, 
+                                             @mainComplaint, 
+                                             @behaviorObservation, 
+                                             @communicationNotes, 
+                                             @sensoryNotes, 
+                                             @socialInteraction, 
+                                             @medicationInUse, 
+                                             @evolutionSummary, 
+                                             @nextSteps
+                                         )
+                                     """;
 
                 await using var cmd = new NpgsqlCommand(query, conn);
                 cmd.Parameters.AddWithValue("date", request.Date);
                 cmd.Parameters.AddWithValue("mainComplaint", request.MainComplaint);
-                cmd.Parameters.AddWithValue("idDoctor", request.IdDoctor);
+                cmd.Parameters.AddWithValue("doctor_cpf", doctorCpf);
                 cmd.Parameters.AddWithValue("idPatient", request.IdPatient);
                 cmd.Parameters.AddWithValue("behaviorObservation", request.BehaviorObservation);
                 cmd.Parameters.AddWithValue("communicationNotes", request.CommunicationNotes);
@@ -194,7 +218,6 @@ namespace ConsulTEA.Controllers
                 cmd.Parameters.AddWithValue("medicationInUse", request.MedicationInUse);
                 cmd.Parameters.AddWithValue("evolutionSummary", request.EvolutionSummary);
                 cmd.Parameters.AddWithValue("nextSteps", request.NextSteps);
-                cmd.Parameters.AddWithValue("createdAt", request.CreatedAt);
 
                 var result = await cmd.ExecuteNonQueryAsync();
 
@@ -209,7 +232,7 @@ namespace ConsulTEA.Controllers
 
         // Updates Appointment by Id
         [HttpPut("update/{id}")]
-        public async Task<IActionResult> InsertNewAppointment(int id, AppointmentInsertRequest request)
+        public async Task<IActionResult> UpdateAppointment(int id, [FromBody] AppointmentUpdateRequest request)
         {
             _logger.LogInformation("Insert Appointment Request");
 
@@ -218,13 +241,17 @@ namespace ConsulTEA.Controllers
                 // Creates/checks db conection
                 await using var conn = await _dbService.GetConnection();
 
-                if (request.IdDoctor == null || request.IdPatient == null)
+                if (request.IdPatient == null)
                     return NotFound("É necessário passar um médico e um paciente");
 
+                var cpfClaim = User.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Jti);
+                if (cpfClaim is null) return Unauthorized("Não está logado como médico");
+
+                var doctorCpf = cpfClaim.Value;
+                
                 var query = """
                     UPDATE bd_dados_exame
                     SET 
-                        id_doctor = @idDoctor,
                         id_patient = @idPatient,
                         date = @date,
                         main_complaint = @mainComplaint,
@@ -234,15 +261,14 @@ namespace ConsulTEA.Controllers
                         social_interaction = @socialInteraction,
                         medication_in_use = @medicationInUse,
                         evolution_summary = @evolutionSummary,
-                        next_steps = @nextSteps,
-                        updated_at = @updatedAt
-                    WHERE id_appointment = @id
+                        next_steps = @nextSteps
+                    WHERE id_appointment = @id AND id_doctor = (SELECT bd_doctor_identification.id_doctor FROM public.bd_doctor_identification WHERE cpf = @doctorCpf)
                 """;
 
                 await using var cmd = new NpgsqlCommand(query, conn);
                 cmd.Parameters.AddWithValue("date", request.Date);
                 cmd.Parameters.AddWithValue("mainComplaint", request.MainComplaint);
-                cmd.Parameters.AddWithValue("idDoctor", request.IdDoctor);
+                cmd.Parameters.AddWithValue("doctorCpf", doctorCpf);
                 cmd.Parameters.AddWithValue("idPatient", request.IdPatient);
                 cmd.Parameters.AddWithValue("behaviorObservation", request.BehaviorObservation);
                 cmd.Parameters.AddWithValue("communicationNotes", request.CommunicationNotes);
@@ -251,7 +277,6 @@ namespace ConsulTEA.Controllers
                 cmd.Parameters.AddWithValue("medicationInUse", request.MedicationInUse);
                 cmd.Parameters.AddWithValue("evolutionSummary", request.EvolutionSummary);
                 cmd.Parameters.AddWithValue("nextSteps", request.NextSteps);
-                cmd.Parameters.AddWithValue("createdAt", request.CreatedAt);
                 cmd.Parameters.AddWithValue("id", id);
 
                 var result = await cmd.ExecuteNonQueryAsync();
--- API/ConsulTEA/Entities/AppointmentRequests.cs
+++ API/ConsulTEA/Entities/AppointmentRequests.cs
@@ -1,5 +1,5 @@
 ﻿namespace ConsulTEA.Entities
 {
-    public record AppointmentInsertRequest(int IdPatient, int IdDoctor, DateTime Date, string MainComplaint, string BehaviorObservation, string CommunicationNotes, string SensoryNotes, string SocialInteraction, string MedicationInUse, string EvolutionSummary, string NextSteps, DateTime CreatedAt);
-    public record AppointmentUpdateRequest(int IdPatient, int IdDoctor, DateTime Date, string MainComplaint, string BehaviorObservation, string CommunicationNotes, string SensoryNotes, string SocialInteraction, string MedicationInUse, string EvolutionSummary, string NextSteps, DateTime CreatedAt);
+    public record AppointmentInsertRequest(int IdPatient, DateTime Date, string MainComplaint, string BehaviorObservation, string CommunicationNotes, string SensoryNotes, string SocialInteraction, string MedicationInUse, string EvolutionSummary, string NextSteps);
+    public record AppointmentUpdateRequest(int IdPatient, DateTime Date, string MainComplaint, string BehaviorObservation, string CommunicationNotes, string SensoryNotes, string SocialInteraction, string MedicationInUse, string EvolutionSummary, string NextSteps);
 }
